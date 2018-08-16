67a40d071fad9eefc9e00019561d320479732a24
Collier Dorian <dorian.c.collier@gmail.com>
2018-08-16 11:40:37 +0200

    IE

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -269,15 +269,13 @@ function issetInfo() {
   });
 }
 
-// test
+
 var version = detectIE();
 
 if (version === false) {
-  document.querySelector('body').innerHTML = "This app does not support Internet Explorer or Edge. Please download Google Chrome or Firefox.";
+} else {
+  document.querySelector("body").innerHTML = "This web app doesn't support Internet Explorer. Please download Google Chrome or Firefox";
 }
-
-// add details to debug result
-document.getElementById('details').innerHTML = window.navigator.userAgent;
 
 /**
  * detect IE
@@ -285,21 +283,6 @@ document.getElementById('details').innerHTML = window.navigator.userAgent;
  */
 function detectIE() {
   var ua = window.navigator.userAgent;
-
-  // Test values; Uncomment to check result …
-
-  // IE 10
-  // ua = 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)';
-  
-  // IE 11
-  // ua = 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko';
-  
-  // Edge 12 (Spartan)
-  // ua = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36 Edge/12.0';
-  
-  // Edge 13
-  // ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586';
-
   var msie = ua.indexOf('MSIE ');
   if (msie > 0) {
     // IE 10 or older => return version number
